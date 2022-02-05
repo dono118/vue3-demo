a44b0ba20bc217f0bdd078214b531783abb72121
dono118 <dono118@126.com>
2022-02-05 21:51:48 +0800

    初识reactive函数

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,51 @@
 <template>
   <h1>个人信息</h1>
-  <h2>姓名：{{name}}</h2>
-  <h2>年龄：{{age}}</h2>
-  <h2>职位：{{job.type}}</h2>
-  <h2>薪水：{{job.salary}}</h2>
+  <h2>姓名：{{person.name}}</h2>
+  <h2>年龄：{{person.age}}</h2>
+  <h2>职位：{{person.job.type}}</h2>
+  <h2>薪水：{{person.job.salary}}</h2>
+  <h2>爱好：{{person.hobbies}}</h2>
+  <h2>测试数据c：{{person.job.a.b.c}}</h2>
   <button @click="changeInfo">修改个人信息</button>
 </template>
 
 <script>
-import { ref } from "vue";
+import { reactive } from "vue";
 
 export default {
   name: 'App',
   setup() {
     // 数据
-    let name = ref('张三')
-    let age = ref(24)
-    let job = ref({
-      type: '前端工程师',
-      salary: '10k'
+    let person = reactive({
+      name: '张三',
+      age: 24,
+      job: {
+        type: '前端工程师',
+        salary: '10k',
+        a: {
+          b: {
+            c: 666
+          }
+        }
+      },
+      hobbies: ['抽烟', '喝酒', '烫头']
     })
+
 
     // 方法
     function changeInfo() {
-      name.value = '李四'
-      age.value = 28
-      // console.log(job.value);
-      job.value.type = 'UI设计师'
-      job.value.salary = '20k'
+      person.name = '李四'
+      person.age = 28
+      // console.log(job);
+      person.job.a.b.c = 999
+      person.job.type = 'UI设计师'
+      person.job.salary = '20k'
+      person.hobbies[0] = '学习'
     }
 
     // 返回一个对象
     return {
-      name,
-      age,
-      job,
+      person,
       changeInfo
     }
   }
