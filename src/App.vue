d4b5117450759e276b0712a9039eda71990cf73f
dono118 <dono118@126.com>
2022-02-06 21:21:38 +0800

    reactive-添加和删除属性

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <h1>个人信息</h1>
-  <h2>姓名：{{person.name}}</h2>
+  <h2 v-show="person.name">姓名：{{person.name}}</h2>
   <h2>年龄：{{person.age}}</h2>
+  <h2 v-show="person.sex">性别：{{person.sex}}</h2>
   <h2>职位：{{person.job.type}}</h2>
   <h2>薪水：{{person.job.salary}}</h2>
   <h2>爱好：{{person.hobbies}}</h2>
   <h2>测试数据c：{{person.job.a.b.c}}</h2>
   <button @click="changeInfo">修改个人信息</button>
+  <button @click="addSex">添加sex属性</button>
+  <button @click="deleteName">删除name属性</button>
 </template>
 
 <script>
@@ -43,10 +46,20 @@ export default {
       person.hobbies[0] = '学习'
     }
 
+    function addSex() {
+      person.sex = '男'
+    }
+
+    function deleteName() {
+      delete person.name
+    }
+
     // 返回一个对象
     return {
       person,
-      changeInfo
+      changeInfo,
+      addSex,
+      deleteName
     }
   }
 }
